0e832e45bdbfa501d50cb265942bbe3e507a3e3c
YongZhen Low <cephyz@gmail.com>
2022-07-30 00:22:30 +0800

    feat: add seo defaults

---
+++ next-seo.config.js
@@ -0,0 +1,15 @@
+const nextSeoConfig = {
+  titleTemplate: '%s — My Site',
+  defaultTitle: 'My Site',
+  description: '',
+  canonical: '',
+  openGraph: {
+    type: 'website',
+    locale: '',
+    url: '',
+    site_name: '',
+    images: [],
+  },
+}
+
+export default nextSeoConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
+import nextSeoConfig from 'next-seo.config'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <DefaultSeo {...nextSeoConfig} />
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
