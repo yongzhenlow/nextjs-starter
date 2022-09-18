b3a5b0534d376232512da33337908d45f6b4f2e0
YongZhen Low <cephyz@gmail.com>
2022-09-19 06:20:22 +0800

    chore(prettier): sort tailwindcss classes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className='container mx-auto h-screen flex items-center justify-center'>
-      <h1 className='text-2xl text-center'>Hello World</h1>
+    <div className="container mx-auto flex h-screen items-center justify-center">
+      <h1 className="text-center text-2xl">Hello World</h1>
     </div>
   )
 }
