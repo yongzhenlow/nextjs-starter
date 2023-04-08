3e55a14b2a8a9fbd8f053cdd60eef59bd00eb9a2
YongZhen Low <cephyz@gmail.com>
2023-04-08 23:21:00 +0800

    chore: update fonts optimisation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,20 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
 import nextSeoConfig from 'next-seo.config'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-  <DefaultSeo {...nextSeoConfig} />
-  <Component {...pageProps} />
-  </>
+  return (
+    <div className={`${inter.variable} font-sans`}>
+      <DefaultSeo {...nextSeoConfig} />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,33 +1,13 @@
-import type { DocumentContext, DocumentInitialProps } from 'next/document'
-import { Head, Html, Main, NextScript } from 'next/document'
-import Document from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx)
-
-    return initialProps
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head>
-          {/**
-           *  Add your custom font style tags here
-           * e.g. <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
-           * @see https://nextjs.org/docs/basic-features/font-optimization
-           */}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
-
-export default MyDocument
